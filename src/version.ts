f86740951dbaffb1198863974f59fc29d40d98a1
Erick Tran <thelittlebotengineer@gmail.com>
2025-08-20 11:34:18 -0700

    build(project): update deps

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,9 @@
 // Auto-generated version file
 // Do not edit manually - this file is updated by scripts/generate-version.js
 
-export const BUILD_VERSION = '25.08.20-dev.1';
-export const BUILD_DATE = '2025-08-20T07:19:28.595Z';
-export const BUILD_TIMESTAMP = 1755674368595;
+export const BUILD_VERSION = '25.08.20-dev.2';
+export const BUILD_DATE = '2025-08-20T18:34:00.867Z';
+export const BUILD_TIMESTAMP = 1755714840867;
 export const BUILD_INFO = {
   version: BUILD_VERSION,
   date: BUILD_DATE,
